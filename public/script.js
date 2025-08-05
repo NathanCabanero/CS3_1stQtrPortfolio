d6ba4cb2a44fdb8c6b4ecffcfeedc95489235b28
NathanCabanero <b2029napcabanero@pshs.edu.ph>
2025-08-05 08:57:45 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
 // no script tag needed here
+
+let age = 14
+let maxAge = 100
+let numPerDay = 1
+totalRequired = (numPerDay * 365) * (maxAge - age);
+
+document.write(" You will need " + totalRequired + " Cheeseburgers to last you until the ripe of old age " + maxAge)// no script tag needed here
